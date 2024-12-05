1bba20684d468877ce7658c5a8118e37094cd5a4
Bryan Sabathino Hursina <2382034@unai.edu>
2024-12-05 23:34:57 +0700

    FInal

---
+++ entities/Customer.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Customer {
+    private String name;
+    private String addreas;
+    private String phone;
+
+    public Customer(String name, String addreas, String phone){
+        this.name = name;
+        this.addreas = addreas;
+        this.phone = phone;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getAddreas() {
+        return addreas;
+    }
+
+    public void setAddreas(String addreas) {
+        this.addreas = addreas;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
